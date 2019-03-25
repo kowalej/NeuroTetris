27b0337c4fd843b9f626d464b6fdad2566ca9b2b
softyy <adkincj@gmail.com>
2019-03-25 00:54:36 -0400

    TetrimoBuilder now builds, and Tetrimo can  now rotate

--- Assets/Scripts/Models/Tetrimo.cs
+++ Assets/Scripts/Models/Tetrimo.cs
@@ -6,17 +6,18 @@ using System.Linq;
 public class Tetrimo : MonoBehaviour
 {
     public Color Colour;
-    public Vector2[] BlockPositions;
+    public int CurrentIndex;
+    public int[][,] BlockPositions;
     // Start is called before the first frame update
     void Start()
     {
-        ApplyColour(Colour);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ApplyColour(Color color)
@@ -27,8 +28,33 @@ public class Tetrimo : MonoBehaviour
         }
     }
 
+    public void ApplyColour()
+    {
+        ApplyColour(Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+    }
+
     public void Rotate()
     {
-        
+        // Check for index wrap
+        CurrentIndex = (CurrentIndex + 1 < BlockPositions.Length) ? CurrentIndex + 1 : 0;
+
+        // Move each block by manipulating it's transform coordinates wrt it's parent.
+        // The world scale is set to unit length
+        int counter = 0;
+        Transform[] ts = this.GetComponentsInChildren<Transform>().Where(x => x.name == "Block").ToArray();
+
+        int[,] layout = BlockPositions[CurrentIndex];
+        for(int i=layout.GetLowerBound(0); i<=layout.GetUpperBound(0);i++)
+            for(int j=layout.GetLowerBound(1); j<=layout.GetUpperBound(1);j++)
+                if (layout[i,j] == 1)
+                {
+                    ts[counter].position = new Vector3(i,j,0);
+                    counter++;
+                }
+    }
+
+    public bool CheckForTetrimoCollision()
+    {
+        throw new System.Exception("Not Yet Implemented");
     }
 }
--- Assets/Scripts/TetrimoBuilder.cs
+++ Assets/Scripts/TetrimoBuilder.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class TetrmioBuilder : MonoBehaviour
+using System.Linq;
+public class TetrimoBuilder : MonoBehaviour
 {
-    private readonly int[][,] TLayouts = {
+    private static readonly int[][,] TLayouts = {
         new int[,] {
             {0,1,0},
             {1,1,1},
@@ -26,7 +26,7 @@ public class TetrmioBuilder : MonoBehaviour
             {0,1,0}
         },
     };
-    private readonly int[][,] LLayouts = {
+    private static readonly int[][,] LLayouts = {
         new int[,] {
             {0,1,0},
             {0,1,0},
@@ -48,7 +48,7 @@ public class TetrmioBuilder : MonoBehaviour
             {0,0,0}
         },
     };
-    private readonly int[][,] JLayouts = {
+    private static readonly int[][,] JLayouts = {
         new int[,] {
             {0,1,0},
             {0,1,0},
@@ -70,7 +70,7 @@ public class TetrmioBuilder : MonoBehaviour
             {0,0,1}
         },
     };
-    private readonly int[][,] ILayouts = {
+    private static readonly int[][,] ILayouts = {
         new int[,] {
             {0,1,0,0},
             {0,1,0,0},
@@ -84,7 +84,7 @@ public class TetrmioBuilder : MonoBehaviour
             {0,0,0,0}
         }
     };
-    private readonly int[][,] SLayouts = {
+    private static readonly int[][,] SLayouts = {
         new int[,] {
             {0,1,1},
             {1,1,0},
@@ -97,7 +97,7 @@ public class TetrmioBuilder : MonoBehaviour
         }
     };
 
-    private readonly int[][,] ZLayouts = {
+    private static readonly int[][,] ZLayouts = {
         new int[,] {
             {1,1,0},
             {0,1,1},
@@ -109,16 +109,73 @@ public class TetrmioBuilder : MonoBehaviour
             {0,1,0}
         }
     };
+    private static readonly int[][,] OLayouts = {
+        new int[,] {
+            {1,1},
+            {1,1}
+        }
+    };
+    public static readonly Dictionary<string,int[][,]> TetrimoLayoutDictionary = new Dictionary<string, int[][,]>(){
+        {"I", ILayouts},
+        {"T", TLayouts},
+        {"L", LLayouts},
+        {"J", JLayouts},
+        {"S", SLayouts},
+        {"Z", ZLayouts},
+        {"O", OLayouts}
+    };
+    public static readonly string[] TetrimoNames = TetrimoLayoutDictionary.Keys.ToArray();
+    
+    public GameObject TetrimoBaseBlock; // Assigned in Unity Editor
+    public Vector2 TetrimoCreationPoint; // Assigned in Unity Editor
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateRandomTetrimo();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreateTetrimo(string tetrimoName)
+    {
+        if (!TetrimoNames.Contains(tetrimoName))
+            throw new MissingComponentException($"There is no tetrimo called {tetrimoName}");
+        CreateTetrimo(tetrimoName,TetrimoLayoutDictionary[tetrimoName].First(),0);    
+    }
+
+    public void CreateTetrimo(string tetrimoName,int[,] tetrimoLayout,int layoutIndex)
+    {
+        GameObject tetrimo = Instantiate(new GameObject("tetrimo"));
+        
+        for(int i=tetrimoLayout.GetLowerBound(0); i<=tetrimoLayout.GetUpperBound(0);i++)
+            for(int j=tetrimoLayout.GetLowerBound(1); j<=tetrimoLayout.GetUpperBound(1);j++)
+                if (tetrimoLayout[i,j] == 1)
+                    drawBaseBlock(i,j,tetrimo.transform);
+        
+        Tetrimo t = tetrimo.AddComponent<Tetrimo>();
+        t.BlockPositions = TetrimoLayoutDictionary[tetrimoName];
+        t.CurrentIndex = layoutIndex;
+        t.ApplyColour();
+    }
+
+    public void CreateRandomTetrimo()
+    {
+        string randomTetrimo = TetrimoNames[Random.Range(0,TetrimoNames.Length)];
+        int randomIndex = Random.Range(0,TetrimoLayoutDictionary[randomTetrimo].Length);
+        int[,] randomLayout = TetrimoLayoutDictionary[randomTetrimo][randomIndex];
+        CreateTetrimo(randomTetrimo,randomLayout,randomIndex);
+    }
+
+    private void drawBaseBlock(int x, int y, Transform parentTransform)
+    {
+        Vector3 v = new Vector3(x,y,0);
+        GameObject baseBlock = Instantiate(TetrimoBaseBlock,v,Quaternion.identity);
+        baseBlock.name = "Block";
+        baseBlock.transform.parent = parentTransform;
     }
 }
